27d906b76dd5e42ec126a58ab31a20b9afd50068
= <=>
2023-12-15 23:35:32 -0300

    Projeto iniciado

---
+++ src/test/java/com/info/grid/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.grid.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
