27a5d1c8587f09a1628c6a9129831719126644a0
= <=>
2023-12-16 16:43:54 -0300

    Alteração no retorno do objeto na classe UserResource de User para UserDTO.

--- src/main/java/com/info/grid/api/resource/UserResource.java
+++ src/main/java/com/info/grid/api/resource/UserResource.java
@@ -1,7 +1,8 @@
 package com.info.grid.api.resource;
 
-import com.info.grid.api.domain.User;
+import com.info.grid.api.domain.dto.UserDTO;
 import com.info.grid.api.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserResource {
-
+    @Autowired
+    private ModelMapper mapper;
     @Autowired
     private UserService userService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(this.userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(this.userService.findById(id), UserDTO.class));
     }
 }
